488251475d36886e4387fe01528c81c62e92e4f8
Tobias Braun <tobiasmarkusbraun@gmail.com>
2022-09-04 10:59:24 +0200

    Add async typeorm factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,15 @@ import { TodoistService } from './todoist/todoist.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import env from './config/dev';
+import { ENV_VARIABLES } from './env/env';
 
 @Module({
   imports: [ConfigModule.forRoot({
     load: [env],
-  }), TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'root',
-    password: 'root',
-    database: 'central_orga_node',
-    entities: [],
-    synchronize: true,
+  }), TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (config: ConfigService) => config.get(ENV_VARIABLES.database),
+    inject: [ConfigService],
   })],
   controllers: [AppController],
   providers: [AppService, TodoistService],
